9a02d3c63b328987f2e1533967409e489afa143b
gscoccola <79168517+gscoccola@users.noreply.github.com>
2022-10-15 19:53:05 -0300

    Delete eea2022_tp1_scoccola_gabriel/eea2022_tp1_scoccola_gabriel directory

--- eea2022_tp1_scoccola_gabriel/eea2022_tp1_scoccola_gabriel/reorder.R
+++
@@ -1,7 +0,0 @@
-```{r}
-p <- h0 %>%
-  mutate(name = fct_relevel(h0$c_verdura,"1 a 3 veces durante los Aºltimos 7 dA-as", "1 vez al dA-a", "2 veces al dA-a", "3 veces al dA-a", "4 a 6 veces durante los Aºltimos 7 dA-as", "4 o mA!s veces al dA-a", "Dato perdido", "No comA- verduras ni hortalizas durante los Aºltimos 7 dA-as")) %>%
-  
-  ggplot(h0, aes(x=h0$c_verdura))
-
-```
